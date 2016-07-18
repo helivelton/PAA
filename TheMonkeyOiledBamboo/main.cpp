c1f2e0b79baebc21febc6d0c5445ea025713d41a
Helivelton Barbosa <helivelton@ic.ufal.br>
2016-07-18 18:01:47 -0300

    TheMonkeyAndTheOiledBamboo

---
+++ TheMonkeyOiledBamboo/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+bool simulation(int energy, const vector<int> &heights, int size) {
+    int distance = 0;
+    for (int i = 0; i < size; ++i) {
+        if ((heights[i] - distance) < energy) {
+            distance += (heights[i] - distance);
+        } else if ((heights[i] - distance) == energy) {
+            distance += (heights[i] - distance);
+            energy--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int main() {
+
+
+    int inputs;
+    scanf("%d", &inputs);
+    vector<vector<int> > matrix;
+
+    int *heights;
+    heights = (int *) malloc(sizeof(int) * (inputs));
+
+    for (int i = 0; i < inputs; ++i) {
+        int size;
+        scanf("%d", &size);
+
+        int maxD = 0;
+        int j;
+        vector<int> newColumn;
+        for (j = 0; j < size; ++j) {
+            int height;
+            scanf("%d", &height);
+            newColumn.push_back(height);
+        }
+
+        matrix.push_back(newColumn);
+
+    }
+
+    for(int i = 0; i<inputs; ++i)
+    {
+
+
+        int middle = matrix.at(i).size()/2;
+        int energy = (matrix.at(i).at(middle)  - matrix.at(i).at((middle - 1)));
+
+        bool pass = simulation(energy, matrix.at(i), matrix.at(i).size());
+
+        if (pass) {
+            while (simulation(energy, matrix.at(i), matrix.at(i).size())) {
+                energy--;
+            }
+                energy++;
+        } else {
+            while (!(simulation(energy, matrix.at(i), matrix.at(i).size()))) {
+                energy++;
+            }
+
+        }
+
+        printf("Case %d: %d\n",(i+1), energy);
+
+
+    }
+    return 0;
+}
